74ae6fd82f2de88cedb28cb6af0e2933deae8655
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-05 00:06:06 -0400

    l10n updates

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_es.dart
@@ -0,0 +1,12 @@
+
+
+
+import 'app_localizations.dart';
+
+/// The translations for Spanish Castilian (`es`).
+class AppLocalizationsEs extends AppLocalizations {
+  AppLocalizationsEs([String locale = 'es']) : super(locale);
+
+  @override
+  String get dashboard => 'Tablero';
+}
