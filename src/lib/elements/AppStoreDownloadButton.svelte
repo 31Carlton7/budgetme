ba4e8a331688471c21e81950a5d6686282a1521e
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-04 23:01:08 -0400

    Create budgetme_website

---
+++ src/lib/elements/AppStoreDownloadButton.svelte
@@ -0,0 +1,7 @@
+<a href="/" class="px-2">
+  <img
+    src="static/images/app_store_download_button.png"
+    alt=""
+    width="175"
+  /></a
+>
---
+++ svelte.config.js
@@ -0,0 +1,8 @@
+import sveltePreprocess from 'svelte-preprocess';
+import preprocess from 'svelte-preprocess';
+
+export default {
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: sveltePreprocess({ postcss: true }),
+};
