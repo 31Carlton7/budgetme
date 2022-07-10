19789e4e6b832d0249ea9f4fd7fd13e435136dd0
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-10 14:49:36 -0400

    Update configuration files

--- budgetme_website/rollup.config.js
+++ budgetme_website/rollup.config.js
@@ -6,6 +6,9 @@ export default {
   plugins: [
     css({ output: 'public/build/vendor.css' }),
     svelte({ include: 'src/**/*.svelte' }),
-    resolve({ browser: true }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte', 'svelte/transition', 'svelte/internal'],
+    }),
   ],
 };
--- budgetme_website/svelte.config.js
+++ budgetme_website/svelte.config.js
@@ -4,6 +4,6 @@ import preprocess from 'svelte-preprocess';
 export default {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: sveltePreprocess({ postcss: true }),
+  preprocess: sveltePreprocess({ css: true }),
   kit: { target: '#svelte' },
 };
