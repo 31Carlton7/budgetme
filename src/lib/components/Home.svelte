705d805eeb42a0ce76de49d3650d28c1c68fdb86
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-10 14:50:24 -0400

    Update pages in site

--- src/lib/components/Home.svelte
+++ src/lib/components/Home.svelte
@@ -1,10 +1,7 @@
 <script>
-  import AppStoreDownloadButton from '../elements/AppStoreDownloadButton.svelte';
   import Banner from '../elements/Banner.svelte';
   import Footer from '../elements/Footer.svelte';
-  import GooglePlayDownloadButton from '../elements/GooglePlayDownloadButton.svelte';
-  import HeroSection from '../elements/HeroSection.svelte';
-  import Logo from '../elements/Logo.svelte';
+  import HeroSection from '../elements/HomePage/HeroSection.svelte';
   import Navbar from '../elements/Navbar.svelte';
 </script>
 
@@ -12,7 +9,7 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 justify-center">
     <Navbar />
     <HeroSection />
-    <div class="mockup py-14 flex items-center justify-center">
+    <div class="py-14 flex items-center justify-center">
       <img src="static/images/mockup_home.png" alt="" width="650" />
     </div>
     <Banner />
