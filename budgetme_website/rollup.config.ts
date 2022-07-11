c6808583aa7069ffa3b453e7d2845e1abdf9c203
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-11 13:51:29 -0400

    Add Rollup to project

---
+++ budgetme_website/rollup.config.ts
@@ -0,0 +1,32 @@
+import svelte from 'rollup-plugin-svelte';
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import sveltePreprocess from 'svelte-preprocess';
+import typescript from '@rollup/plugin-typescript';
+import postcss from 'rollup-plugin-postcss';
+
+export default {
+  input: './src/main.ts',
+  output: {
+    format: 'iife',
+    file: './build/bundle.js',
+  },
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+    resolve({ browser: true }),
+    commonjs(),
+    typescript(),
+    postcss({
+      minimize: true,
+      modules: true,
+      use: {
+        sass: null,
+        stylus: null,
+        less: { javascriptEnabled: true },
+      },
+      extract: true,
+    }),
+  ],
+};
