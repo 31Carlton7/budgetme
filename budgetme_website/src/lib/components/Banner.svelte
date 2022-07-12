8559819e06ff098450090eb9bf0ec620f6f0dc92
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 12:15:17 -0400

    Align banner text in center

--- budgetme_website/src/lib/components/Banner.svelte
+++ budgetme_website/src/lib/components/Banner.svelte
@@ -1,7 +1,7 @@
 <div
   class="bg-gradient-to-b from-primary-500 to-primary-600 w-screen left-1/2 right-1/2 -mr-[50vw] -ml-[50vw] relative flex py-20 items-center justify-center"
 >
-  <h1 class="font-bold md:text-6xl text-3xl text-white">
+  <h1 class="font-bold md:text-6xl text-3xl text-white text-center">
     Download BudgetMe Today!
   </h1>
 </div>
