6b144939acb39cd85d47a86bd8b72ceb60c018e9
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:49:35 -0400

    Create License Headers for Web version

--- budgetme_website/src/lib/components/Logo.svelte
+++ budgetme_website/src/lib/components/Logo.svelte
@@ -1,3 +1,21 @@
+<!--
+BudgetMe iOS & Android App
+Copyright (C) 2022 Carlton Aikins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <div class="flex items-center">
   <img src="images/icon.png" alt="" width="30" height="auto" />
   <h1 class="px-2 font-semibold text-xl">BudgetMe</h1>
