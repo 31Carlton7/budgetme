1184888910407663e6727a6fc712b0700a349081
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:06:51 -0400

    Fix Privacy Policy Page

--- budgetme_website/src/routes/PrivacyPolicy.svelte
+++ budgetme_website/src/routes/PrivacyPolicy.svelte
@@ -52,24 +52,6 @@
       ]}
     />
     <PrivacyPolicySection
-      title="Privacy Policy"
-      content={[
-        'Carlton Aikins built the BudgetMe app as an Ad Supported app. This SERVICE is provided by Carlton Aikins at no cost and is intended for use as is.',
-        'This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.',
-        'If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.',
-        'The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at BudgetMe unless otherwise defined in this Privacy Policy.',
-      ]}
-    />
-    <PrivacyPolicySection
-      title="Privacy Policy"
-      content={[
-        'Carlton Aikins built the BudgetMe app as an Ad Supported app. This SERVICE is provided by Carlton Aikins at no cost and is intended for use as is.',
-        'This page is used to inform visitors regarding my policies with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.',
-        'If you choose to use my Service, then you agree to the collection and use of information in relation to this policy. The Personal Information that I collect is used for providing and improving the Service. I will not use or share your information with anyone except as described in this Privacy Policy.',
-        'The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which are accessible at BudgetMe unless otherwise defined in this Privacy Policy.',
-      ]}
-    />
-    <PrivacyPolicySection
       title="Security"
       content={[
         'I value your trust in providing us your Personal Information, thus we are striving to use commercially acceptable means of protecting it. But remember that no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its absolute security.',
