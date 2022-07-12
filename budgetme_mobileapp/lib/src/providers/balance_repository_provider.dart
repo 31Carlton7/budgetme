795a668daa1dadb2dc669f639fea9f8bf8bb7930
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:32:22 -0400

    Sort imports

--- budgetme_mobileapp/lib/src/providers/balance_repository_provider.dart
+++ budgetme_mobileapp/lib/src/providers/balance_repository_provider.dart
@@ -16,8 +16,11 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:budgetme/src/repositories/balance_repository.dart';
+// Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Project imports:
+import 'package:budgetme/src/repositories/balance_repository.dart';
 
 final balanceRepositoryProvider = ChangeNotifierProvider<BalanceRepository>((ref) {
   return BalanceRepository();
