b3100edbd2eb446b40317894b55388ca14c9186e
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:33:05 -0400

    Sort imports

--- budgetme_mobileapp/lib/src/ui/views/dashboard_view/components/goal_card.dart
+++ budgetme_mobileapp/lib/src/ui/views/dashboard_view/components/goal_card.dart
@@ -16,18 +16,23 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+// Dart imports:
 import 'dart:math';
 
+// Flutter imports:
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:heroicons/heroicons.dart';
+
+// Project imports:
 import 'package:budgetme/src/config/constants.dart';
 import 'package:budgetme/src/config/themes/light_theme/light_color_palette.dart';
 import 'package:budgetme/src/models/goal.dart';
 import 'package:budgetme/src/ui/components/box_shadow.dart';
 import 'package:budgetme/src/ui/views/goal_view/components/show_goal_settings_bottom_sheet.dart';
 import 'package:budgetme/src/ui/views/goal_view/goal_view.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:heroicons/heroicons.dart';
 
 class GoalCard extends ConsumerWidget {
   const GoalCard(this.goal, this.setState, {Key? key}) : super(key: key);
