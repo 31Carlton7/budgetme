2985ae649566ba68ac96d4699d04d6cdc5d2097f
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:49:58 -0400

    Create License Headers for Web version

--- budgetme_website/src/lib/components/SupportPage/TwitterCard.svelte
+++ budgetme_website/src/lib/components/SupportPage/TwitterCard.svelte
@@ -1,3 +1,21 @@
+<!--
+BudgetMe iOS & Android App
+Copyright (C) 2022 Carlton Aikins
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-->
+
 <script lang="ts">
   const twitterSvgPath: string =
     'M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84';
