8e8da4ef11c7f10d7c9437cca50b75ef3868e448
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:33:12 -0400

    Sort imports

--- budgetme_mobileapp/lib/src/ui/views/goal_view/components/show_goal_settings_bottom_sheet.dart
+++ budgetme_mobileapp/lib/src/ui/views/goal_view/components/show_goal_settings_bottom_sheet.dart
@@ -16,20 +16,23 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-import 'package:budgetme/src/lang/budgetme_localizations.dart';
+// Flutter imports:
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// Package imports:
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:heroicons/heroicons.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+// Project imports:
 import 'package:budgetme/src/config/constants.dart';
 import 'package:budgetme/src/config/themes/light_theme/light_color_palette.dart';
+import 'package:budgetme/src/lang/budgetme_localizations.dart';
 import 'package:budgetme/src/models/goal.dart';
 import 'package:budgetme/src/providers/goal_repository_provider.dart';
 import 'package:budgetme/src/ui/views/create_goal_view/create_goal_view.dart';
-import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:heroicons/heroicons.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 Future<void> showGoalSettingsBottomSheet(BuildContext ctx, void Function(void Function()) setState, Goal goal) async {
   await showCustomModalBottomSheet(
