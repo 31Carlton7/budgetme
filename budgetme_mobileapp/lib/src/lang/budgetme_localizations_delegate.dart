7528c4f3b11e1be485e1077f5dcaca9d976be01a
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 14:32:13 -0400

    Sort imports

--- budgetme_mobileapp/lib/src/lang/budgetme_localizations_delegate.dart
+++ budgetme_mobileapp/lib/src/lang/budgetme_localizations_delegate.dart
@@ -1,7 +1,10 @@
+// Dart imports:
 import 'dart:async';
 
+// Flutter imports:
 import 'package:flutter/material.dart';
 
+// Project imports:
 import 'budgetme_localizations.dart';
 
 class BudgetMeLocalizationsDelegate extends LocalizationsDelegate<BudgetMeLocalizations> {
