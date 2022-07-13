198b3fc774b70bb58852191e89f182aea5615254
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-12 21:05:34 -0400

    Update translations

---
+++ budgetme_mobileapp/lib/l10n/_messages_all.dart
@@ -0,0 +1,104 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that looks up messages for specific locales by
+// delegating to the appropriate library.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:implementation_imports, file_names
+// ignore_for_file:unnecessary_brace_in_string_interps, directives_ordering
+// ignore_for_file:argument_type_not_assignable, invalid_assignment
+// ignore_for_file:prefer_single_quotes, prefer_generic_function_type_aliases
+// ignore_for_file:comment_references
+// ignore_for_file:avoid_catches_without_on_clauses
+
+import 'dart:async';
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+import 'package:intl/src/intl_helpers.dart';
+
+import '_messages_ko.dart' deferred as messages_ko;
+import '_messages_es.dart' deferred as messages_es;
+import '_messages_he.dart' deferred as messages_he;
+import '_messages_pt.dart' deferred as messages_pt;
+import '_messages_de.dart' deferred as messages_de;
+import '_messages_ar.dart' deferred as messages_ar;
+import '_messages_zh.dart' deferred as messages_zh;
+import '_messages_ja.dart' deferred as messages_ja;
+import '_messages_en.dart' deferred as messages_en;
+import '_messages_ru.dart' deferred as messages_ru;
+import '_messages_fr.dart' deferred as messages_fr;
+
+typedef Future<dynamic> LibraryLoader();
+Map<String, LibraryLoader> _deferredLibraries = {
+  'ko': messages_ko.loadLibrary,
+  'es': messages_es.loadLibrary,
+  'he': messages_he.loadLibrary,
+  'pt': messages_pt.loadLibrary,
+  'de': messages_de.loadLibrary,
+  'ar': messages_ar.loadLibrary,
+  'zh': messages_zh.loadLibrary,
+  'ja': messages_ja.loadLibrary,
+  'en': messages_en.loadLibrary,
+  'ru': messages_ru.loadLibrary,
+  'fr': messages_fr.loadLibrary,
+};
+
+MessageLookupByLibrary? _findExact(String localeName) {
+  switch (localeName) {
+    case 'ko':
+      return messages_ko.messages;
+    case 'es':
+      return messages_es.messages;
+    case 'he':
+      return messages_he.messages;
+    case 'pt':
+      return messages_pt.messages;
+    case 'de':
+      return messages_de.messages;
+    case 'ar':
+      return messages_ar.messages;
+    case 'zh':
+      return messages_zh.messages;
+    case 'ja':
+      return messages_ja.messages;
+    case 'en':
+      return messages_en.messages;
+    case 'ru':
+      return messages_ru.messages;
+    case 'fr':
+      return messages_fr.messages;
+    default:
+      return null;
+  }
+}
+
+/// User programs should call this before using [localeName] for messages.
+Future<bool> initializeMessages(String localeName) async {
+  final availableLocale = Intl.verifiedLocale(
+    localeName,
+    (locale) => _deferredLibraries[locale] != null,
+    onFailure: (_) => null);
+  if (availableLocale == null) {
+    return Future.value(false);
+  }
+  final lib = _deferredLibraries[availableLocale];
+  await (lib == null ? Future.value(false) : lib());
+  initializeInternalMessageLookup(() => CompositeMessageLookup());
+  messageLookup.addLocale(availableLocale, _findGeneratedMessagesFor);
+  return Future.value(true);
+}
+
+bool _messagesExistFor(String locale) {
+  try {
+    return _findExact(locale) != null;
+  } catch (e) {
+    return false;
+  }
+}
+
+MessageLookupByLibrary? _findGeneratedMessagesFor(String locale) {
+  final actualLocale = Intl.verifiedLocale(locale, _messagesExistFor,
+      onFailure: (_) => null);
+  if (actualLocale == null) return null;
+  return _findExact(actualLocale);
+}
