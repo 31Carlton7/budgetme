fb2bcb9da9d5ca0813e68b30f181576ae93f4c0f
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-13 14:23:44 -0400

    Increment Build Number to v1.0.3(4)

--- budgetme_mobileapp/lib/src/config/constants.dart
+++ budgetme_mobileapp/lib/src/config/constants.dart
@@ -33,7 +33,7 @@ import 'package:budgetme/src/models/goal.dart';
 
 const String kAppTitle = 'BudgetMe';
 
-const String kVersionNumber = 'v1.0.1 (2)';
+const String kAppVersionNumber = 'v1.0.3 (4)';
 
 const String kFontFamily = 'Inter';
 
