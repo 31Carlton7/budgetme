552bc48e170dd01341aaf877a1d49cf8d95743e7
Carlton Aikins <carltonaikins7@gmail.com>
2022-07-16 21:52:01 -0400

    Update ../lib/l10n/messages_fr.dart

--- budgetme_mobileapp/lib/l10n/messages_fr.dart
+++ budgetme_mobileapp/lib/l10n/messages_fr.dart
@@ -4,7 +4,7 @@
 // function name.
 
 // Ignore issues from commonly used lints in this file.
-// ignore_for_file:unnecessary_brace_in_string_interps
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_string_escapes
 // ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
 // ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
 // ignore_for_file:unused_import, file_names, always_declare_return_types
@@ -21,84 +21,91 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static m0(goalTitle) => "Êtes-vous sûr de supprimer votre objectif?";
 
-  static m1(currency, number) => "Veuillez saisir une valeur inférieure à votre montant requis de ${currency}​​${number}";
+  static m1(currency, number) =>
+      "Veuillez saisir une valeur inférieure à votre montant requis de ${currency}​​${number}";
 
-  static m2(amount) => "Déverrouillez les objectifs illimités pour seulement 2,99 USD 😆 Payez une fois, payez pour toujours!";
+  static m2(amount) =>
+      "Déverrouillez les objectifs illimités pour seulement 2,99 USD 😆 Payez une fois, payez pour toujours!";
 
   static m3(month) => "Statistiques de juillet";
 
-  static m4(amount) => "Déverrouillez votre vrai moi avec des objectifs illimités pour un achat unique de seulement 2,99 USD! 😎 Remarque: les prix peuvent varier dans d\'autres devises";
+  static m4(amount) =>
+      "Déverrouillez votre vrai moi avec des objectifs illimités pour un achat unique de seulement 2,99 USD! 😎 Remarque: les prix peuvent varier dans d'autres devises";
 
   static m5(percent) => "Vous êtes ${percent} pour cent là-bas!";
 
   static m6(number) => "Version ${number}";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
-  static Map<String, Function> _notInlinedMessages(_) => <String, Function> {
-    "about" : MessageLookupByLibrary.simpleMessage("À propos de"),
-    "add" : MessageLookupByLibrary.simpleMessage("Ajouter"),
-    "adjustMonthlySpendingLimit" : MessageLookupByLibrary.simpleMessage("Ajuster la limite de dépenses mensuelles"),
-    "areYouSureDelQ" : m0,
-    "cancel" : MessageLookupByLibrary.simpleMessage("Annuler"),
-    "congratulations" : MessageLookupByLibrary.simpleMessage("Félicitations pour avoir atteint votre objectif! 😆"),
-    "create" : MessageLookupByLibrary.simpleMessage("Créer"),
-    "currentProgress" : MessageLookupByLibrary.simpleMessage("Progrès en cours"),
-    "dashboard" : MessageLookupByLibrary.simpleMessage("Tableau de bord"),
-    "days" : MessageLookupByLibrary.simpleMessage("Journées"),
-    "delete" : MessageLookupByLibrary.simpleMessage("Effacer"),
-    "deleteGoal" : MessageLookupByLibrary.simpleMessage("Supprimer l\'objectif"),
-    "deleteGoalQ" : MessageLookupByLibrary.simpleMessage("Supprimer l\'objectif?"),
-    "device" : MessageLookupByLibrary.simpleMessage("Dispositif"),
-    "done" : MessageLookupByLibrary.simpleMessage("Fait"),
-    "edit" : MessageLookupByLibrary.simpleMessage("Éditer"),
-    "editGoal" : MessageLookupByLibrary.simpleMessage("Modifier l\'objectif"),
-    "finish" : MessageLookupByLibrary.simpleMessage("Finir"),
-    "hideFrom" : MessageLookupByLibrary.simpleMessage("Se cacher de l\'histoire"),
-    "hours" : MessageLookupByLibrary.simpleMessage("Heures"),
-    "howMuchNeededQ" : MessageLookupByLibrary.simpleMessage("Combien d\'argent est nécessaire?"),
-    "howMuchSavedQ" : MessageLookupByLibrary.simpleMessage("Combien d\'argent est actuellement économisé?"),
-    "minutes" : MessageLookupByLibrary.simpleMessage("Minutes"),
-    "monthlySavings" : MessageLookupByLibrary.simpleMessage("Économies mensuelles"),
-    "months" : MessageLookupByLibrary.simpleMessage("Mois"),
-    "newMonthlySpendingLimit" : MessageLookupByLibrary.simpleMessage("Nouvelle limite de dépenses mensuelles"),
-    "next" : MessageLookupByLibrary.simpleMessage("Prochain"),
-    "noResults" : MessageLookupByLibrary.simpleMessage("Aucun résultat"),
-    "noTransactions" : MessageLookupByLibrary.simpleMessage("Aucune transaction n\'a été effectuée ... pourtant!"),
-    "pleaseEnterLess" : m1,
-    "pleaseNote" : MessageLookupByLibrary.simpleMessage("Veuillez noter que l\'augmentation de vos dépenses hebdomadaires rendra plus difficile l\'atteinte d\'objectifs et qu\'elle est découragée."),
-    "prefCurr" : MessageLookupByLibrary.simpleMessage("Sélectionnez votre devise préférée"),
-    "pressThePBtn" : MessageLookupByLibrary.simpleMessage("Appuyez sur le bouton violet pour créer un nouvel objectif!"),
-    "privacyPolicy" : MessageLookupByLibrary.simpleMessage("Politique de confidentialité"),
-    "profile" : MessageLookupByLibrary.simpleMessage("Profil"),
-    "purchase" : MessageLookupByLibrary.simpleMessage("Acheter"),
-    "purchasePro" : MessageLookupByLibrary.simpleMessage("Acheter pro😎"),
-    "purchaseProText" : m2,
-    "remainingBalance" : MessageLookupByLibrary.simpleMessage("Solde restant"),
-    "remove" : MessageLookupByLibrary.simpleMessage("Retirer"),
-    "save" : MessageLookupByLibrary.simpleMessage("sauvegarder"),
-    "savingForQ" : MessageLookupByLibrary.simpleMessage("Pourquoi économisez-vous?"),
-    "savingsGoals" : MessageLookupByLibrary.simpleMessage("Objectifs d\'épargne"),
-    "search" : MessageLookupByLibrary.simpleMessage("Chercher"),
-    "searchForImage" : MessageLookupByLibrary.simpleMessage("Recherchez des images sur unclash à l\'aide de la barre de recherche"),
-    "seconds" : MessageLookupByLibrary.simpleMessage("Secondes"),
-    "seeAll" : MessageLookupByLibrary.simpleMessage("Voir tout"),
-    "selectImage" : MessageLookupByLibrary.simpleMessage("Sélectionner l\'image"),
-    "settings" : MessageLookupByLibrary.simpleMessage("Réglages"),
-    "skip" : MessageLookupByLibrary.simpleMessage("Sauter"),
-    "startNewGoal" : MessageLookupByLibrary.simpleMessage("Commencer un nouvel objectif"),
-    "statistics" : m3,
-    "support" : MessageLookupByLibrary.simpleMessage("Soutien"),
-    "timeLeft" : MessageLookupByLibrary.simpleMessage("Temps restant"),
-    "totalSaved" : MessageLookupByLibrary.simpleMessage("Total sauvé"),
-    "transaction" : MessageLookupByLibrary.simpleMessage("Transaction"),
-    "transactionHistory" : MessageLookupByLibrary.simpleMessage("Historique des transactions"),
-    "unlimitedGoals" : MessageLookupByLibrary.simpleMessage("Objectifs illimités"),
-    "unlockText" : m4,
-    "userPercentProgress" : m5,
-    "vacation" : MessageLookupByLibrary.simpleMessage("Vacances"),
-    "version" : m6,
-    "web" : MessageLookupByLibrary.simpleMessage("la toile"),
-    "whenDeadlineQ" : MessageLookupByLibrary.simpleMessage("Quand est la date limite?"),
-    "years" : MessageLookupByLibrary.simpleMessage("Années")
-  };
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+        "about": MessageLookupByLibrary.simpleMessage("À propos de"),
+        "add": MessageLookupByLibrary.simpleMessage("Ajouter"),
+        "adjustMonthlySpendingLimit": MessageLookupByLibrary.simpleMessage("Ajuster la limite de dépenses mensuelles"),
+        "areYouSureDelQ": m0,
+        "cancel": MessageLookupByLibrary.simpleMessage("Annuler"),
+        "congratulations": MessageLookupByLibrary.simpleMessage("Félicitations pour avoir atteint votre objectif! 😆"),
+        "create": MessageLookupByLibrary.simpleMessage("Créer"),
+        "currentProgress": MessageLookupByLibrary.simpleMessage("Progrès en cours"),
+        "dashboard": MessageLookupByLibrary.simpleMessage("Tableau de bord"),
+        "days": MessageLookupByLibrary.simpleMessage("Journées"),
+        "delete": MessageLookupByLibrary.simpleMessage("Effacer"),
+        "deleteGoal": MessageLookupByLibrary.simpleMessage("Supprimer l'objectif"),
+        "deleteGoalQ": MessageLookupByLibrary.simpleMessage("Supprimer l'objectif?"),
+        "device": MessageLookupByLibrary.simpleMessage("Dispositif"),
+        "done": MessageLookupByLibrary.simpleMessage("Fait"),
+        "edit": MessageLookupByLibrary.simpleMessage("Éditer"),
+        "editGoal": MessageLookupByLibrary.simpleMessage("Modifier l'objectif"),
+        "finish": MessageLookupByLibrary.simpleMessage("Finir"),
+        "hideFrom": MessageLookupByLibrary.simpleMessage("Se cacher de l'histoire"),
+        "hours": MessageLookupByLibrary.simpleMessage("Heures"),
+        "howMuchNeededQ": MessageLookupByLibrary.simpleMessage("Combien d'argent est nécessaire?"),
+        "howMuchSavedQ": MessageLookupByLibrary.simpleMessage("Combien d'argent est actuellement économisé?"),
+        "minutes": MessageLookupByLibrary.simpleMessage("Minutes"),
+        "monthlySavings": MessageLookupByLibrary.simpleMessage("Économies mensuelles"),
+        "months": MessageLookupByLibrary.simpleMessage("Mois"),
+        "newMonthlySpendingLimit": MessageLookupByLibrary.simpleMessage("Nouvelle limite de dépenses mensuelles"),
+        "next": MessageLookupByLibrary.simpleMessage("Prochain"),
+        "noResults": MessageLookupByLibrary.simpleMessage("Aucun résultat"),
+        "noTransactions": MessageLookupByLibrary.simpleMessage("Aucune transaction n'a été effectuée ... pourtant!"),
+        "outOfTime": MessageLookupByLibrary.simpleMessage("Hors du temps!"),
+        "pleaseEnterLess": m1,
+        "pleaseNote": MessageLookupByLibrary.simpleMessage(
+            "Veuillez noter que l\'augmentation de vos dépenses hebdomadaires rendra plus difficile l\'atteinte d\'objectifs et qu\'elle est découragée."),
+        "prefCurr": MessageLookupByLibrary.simpleMessage("Sélectionnez votre devise préférée"),
+        "pressThePBtn":
+            MessageLookupByLibrary.simpleMessage("Appuyez sur le bouton violet pour créer un nouvel objectif!"),
+        "privacyPolicy": MessageLookupByLibrary.simpleMessage("Politique de confidentialité"),
+        "profile": MessageLookupByLibrary.simpleMessage("Profil"),
+        "purchase": MessageLookupByLibrary.simpleMessage("Acheter"),
+        "purchasePro": MessageLookupByLibrary.simpleMessage("Acheter pro😎"),
+        "purchaseProText": m2,
+        "remainingBalance": MessageLookupByLibrary.simpleMessage("Solde restant"),
+        "remove": MessageLookupByLibrary.simpleMessage("Retirer"),
+        "save": MessageLookupByLibrary.simpleMessage("sauvegarder"),
+        "savingForQ": MessageLookupByLibrary.simpleMessage("Pourquoi économisez-vous?"),
+        "savingsGoals": MessageLookupByLibrary.simpleMessage("Objectifs d\'épargne"),
+        "search": MessageLookupByLibrary.simpleMessage("Chercher"),
+        "searchForImage": MessageLookupByLibrary.simpleMessage(
+            "Recherchez des images sur Unsplash à l\'aide de la barre de recherche"),
+        "seconds": MessageLookupByLibrary.simpleMessage("Secondes"),
+        "seeAll": MessageLookupByLibrary.simpleMessage("Voir tout"),
+        "selectImage": MessageLookupByLibrary.simpleMessage("Sélectionner l\'image"),
+        "settings": MessageLookupByLibrary.simpleMessage("Réglages"),
+        "skip": MessageLookupByLibrary.simpleMessage("Sauter"),
+        "startNewGoal": MessageLookupByLibrary.simpleMessage("Commencer un nouvel objectif"),
+        "statistics": m3,
+        "support": MessageLookupByLibrary.simpleMessage("Soutien"),
+        "timeLeft": MessageLookupByLibrary.simpleMessage("Temps restant"),
+        "totalSaved": MessageLookupByLibrary.simpleMessage("Total sauvé"),
+        "transaction": MessageLookupByLibrary.simpleMessage("Transaction"),
+        "transactionHistory": MessageLookupByLibrary.simpleMessage("Historique des transactions"),
+        "unlimitedGoals": MessageLookupByLibrary.simpleMessage("Objectifs illimités"),
+        "unlockText": m4,
+        "userPercentProgress": m5,
+        "vacation": MessageLookupByLibrary.simpleMessage("Vacances"),
+        "version": m6,
+        "web": MessageLookupByLibrary.simpleMessage("la toile"),
+        "whenDeadlineQ": MessageLookupByLibrary.simpleMessage("Quand est la date limite?"),
+        "years": MessageLookupByLibrary.simpleMessage("Années")
+      };
 }
