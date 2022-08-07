48cd2c8e763f42278930e6e8a7bfebbc3bf0899c
Carlton Aikins <carltonaikins7@gmail.com>
2022-08-07 00:27:53 -0400

    Update ../lib/src/config/device_preview_screenshot_helper.dart

--- budgetme_mobileapp/lib/src/config/device_preview_screenshot_helper.dart
+++ budgetme_mobileapp/lib/src/config/device_preview_screenshot_helper.dart
@@ -1,10 +1,15 @@
 // ignore_for_file: lines_longer_than_80_chars
+
+// Dart imports:
 import 'dart:io';
 
-import 'package:device_preview_screenshot/device_preview_screenshot.dart';
+// Flutter imports:
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+
+// Package imports:
+import 'package:device_preview_screenshot/device_preview_screenshot.dart';
 import 'package:image/image.dart' as img;
 import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
