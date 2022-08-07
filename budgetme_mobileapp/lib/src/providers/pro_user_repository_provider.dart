0e3ec77558e1932ab52036b8de123ac9809510b8
Carlton Aikins <carltonaikins7@gmail.com>
2022-08-07 00:28:06 -0400

    Update ../lib/src/providers/pro_user_repository_provider.dart

--- budgetme_mobileapp/lib/src/providers/pro_user_repository_provider.dart
+++ budgetme_mobileapp/lib/src/providers/pro_user_repository_provider.dart
@@ -1,5 +1,8 @@
-import 'package:budgetme/src/repositories/pro_user_repository.dart';
+// Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Project imports:
+import 'package:budgetme/src/repositories/pro_user_repository.dart';
 
 final proUserRepositoryProvider = ChangeNotifierProvider<ProUserRepository>((ref) {
   return ProUserRepository();
